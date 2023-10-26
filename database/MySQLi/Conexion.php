94b6f8c9be81ad2f66c58e27741f8c54355f9842
Isidoro Torrico M <itorrico.m@gmail.com>
2023-10-26 16:32:41 -0400

    Creando conexion a db

---
+++ database/MySQLi/Conexion.php
@@ -0,0 +1,20 @@
+<?php
+
+$server = "localhost";
+$database = "finanzas_personales";
+$username = "root";
+$password = "";
+
+//Forma procedural
+//$mysqli = $mysqli_connect($server, $username, $password, $database)
+
+//Orientada a objetos
+$mysqli = new $mysqli($server, $username, $password, $database);
+
+//comprobar conexion de manera procedural
+/*if($mysqli)
+    die("Fallo la conexion"+$mysqli_connect_error());*/
+
+//Comprobar conexion de manera orientada a obejetos
+if($mysqli->connect_error)
+    die("Fallos la conexion: {$mysqli->connect_error}");
