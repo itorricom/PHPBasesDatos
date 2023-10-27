c479912bf8d8c8567426b1205f91d9e88b6c5621
Isidoro Torrico <itorrico.m@gmail.com>
2023-10-26 21:39:10 -0400

    Conexion con MySQLi

--- database/MySQLi/Conexion.php
+++ database/MySQLi/Conexion.php
@@ -9,12 +9,17 @@ $password = "";
 //$mysqli = $mysqli_connect($server, $username, $password, $database)
 
 //Orientada a objetos
-$mysqli = new $mysqli($server, $username, $password, $database);
-
+$mysqli = new mysqli($server, $username, $password, $database);
 //comprobar conexion de manera procedural
 /*if($mysqli)
     die("Fallo la conexion"+$mysqli_connect_error());*/
 
 //Comprobar conexion de manera orientada a obejetos
-if($mysqli->connect_error)
+if($mysqli->connect_errno)
     die("Fallos la conexion: {$mysqli->connect_error}");
+
+//Esto noy ayuda a poder usar cualquier caracter de nuestras consultas
+$setnames = $mysqli->prepare("SET NAMES 'utf8'");
+$setnames->execute();
+
+var_dump($setnames);
