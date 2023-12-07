8d88ab455e0e5564e4f2a42f42ae5cf6f7754000
Isidoro <itorrico.m@gmail.com>
2023-12-06 23:39:49 -0400

    Fix convertiendo nuestra conexion a una clase

--- database/PDO/Connection.php
+++ database/PDO/Connection.php
@@ -1,14 +1,40 @@
 <?php
-$server = "localhost";
-$database = "finanzas_personales";
-$username = "root";
-$password = "";
 
-$connection = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+class Connection{
+    private static $instance;
+    private $connection; 
 
-$setNames = $connection->prepare("SET NAMES 'utf8'");
-$setNames->execute();
+    private function __construct(){
+        $this->make_conection();
+    }
 
-var_dump($setNames);
+    public static function getInstance(){
+        if(!self::$instance instanceof self)
+            self::$instance = new self();
+        return self::$instance;
+    }
+
+    private function make_conection()
+    {
+        $server = "localhost";
+        $database = "finanzas_personales";
+        $username = "root";
+        $password = "";
+
+        $mysqli = new mysqli($server, $username, $password, $database);
+        //Comprobar conexion de manera orientada a obejetos
+        if ($mysqli->connect_errno)
+        die("Fallos la conexion: {$mysqli->connect_error}");
+        //Esto noy ayuda a poder usar cualquier caracter de nuestras consultas
+        $setnames = $mysqli->prepare("SET NAMES 'utf8'");
+        $setnames->execute();
+        $this->connection = $mysqli;
+    }
+
+}
+
+
+
+
 
 ?>
